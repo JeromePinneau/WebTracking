75035d356dff93c1439d8abbbdd04a2206daeb8b
Jerome Pinneau <jerome@pinneau.com>
2020-09-08 08:38:19 +0200

    Unsubscribe
    + Unsubscribe page
    + Change default layout

---
+++ Controllers/UnsubscribeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webtracking.Controllers
+{
+    public class UnsubscribeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
